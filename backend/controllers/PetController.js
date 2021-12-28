238e5a42b01ab4de0b4c5e3e38f97ccd991e3714
vtrolinux <vtrolinux@gmail.com>
2021-12-28 16:21:51 -0300

    pet checktoken in route create added

--- backend/controllers/PetController.js
+++ backend/controllers/PetController.js
@@ -2,6 +2,6 @@ module.exports = class PetController {
     constructor(){}
 
     static async create(req, res){
-        
+        return res.json({ message: 'cadastrou seu pet!'})
     }
 }
--- backend/routes/petRoutes.js
+++ backend/routes/petRoutes.js
@@ -1,6 +1,8 @@
 const router = require('express').Router()
 const PetController = require('../controllers/PetController')
+//midlewares
+const checkToken = require('../middlewares/check-token')
 
-router.post('/create', PetController.create)
+router.post('/create', checkToken, PetController.create)
 
 module.exports = router
