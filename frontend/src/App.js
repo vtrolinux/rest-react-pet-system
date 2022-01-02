bd9f25be0dc377c619936bdb1bbae051e96fc707
vtrolinux <vtrolinux@gmail.com>
2022-01-02 06:06:03 -0300

    auth routes

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,26 @@
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
 
+//pages
+import Login from './components/pages/Auth/login'
+import Register from './components/pages/Auth/register'
+import Home from './components/pages/Home'
 
 function App() {
   return (
-    <div className="App">
-      <h1>pet system</h1>
-    </div>
-  );
+    <Router>
+      <Switch>
+        <Route path='/login'>
+          <Login />
+        </Route>
+        <Route path='/register'>
+          <Register />
+        </Route>
+        <Route path='/'>
+          <Home />
+        </Route>
+      </Switch>
+    </Router>
+  )
 }
 
 export default App;
