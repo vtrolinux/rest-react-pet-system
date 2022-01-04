af302c147ced0de0fa5063f7c9c4e4fe7896b7b5
vtrolinux <vtrolinux@gmail.com>
2022-01-04 06:48:50 -0300

    save token on local storage

--- frontend/src/hooks/useAuth.js
+++ frontend/src/hooks/useAuth.js
@@ -4,19 +4,20 @@ import { useHistory } from 'react-router-dom'
 import  useFlashMessage  from './useFlashMessage'
 
 export default function useAuth(){
-
+    const [ authenticated, setAuthenticated] = useState(false)// inicializacao do estado de autenticacao
     const { setFlashMessage } = useFlashMessage()
+    const history = useHistory()
 
     async function register(user){
 
         let msgMessage = 'Cadastro realizado com sucesso'
         let msgType = 'success'
         try {
-            const data = await api.post('/auth/register', user).then((response) => {
-                return response.data
-            })
+            const data = await api.post('/auth/register', user).then((response) => { return response.data })
             
             console.log(data)
+            await authUser(data)
+
         } catch (error) {
             //console.log(error)
             msgMessage = error.response.data.message
@@ -25,6 +26,12 @@ export default function useAuth(){
 
         setFlashMessage(msgMessage, msgType)
 
+    }
+    async function authUser(data){
+
+        setAuthenticated(true)
+        localStorage.setItem('token', JSON.stringify(data.token))
+        history.push('/')
     }
     return { register }
 }
