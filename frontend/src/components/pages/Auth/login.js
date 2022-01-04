dc0a6d761c90d891ab11a901715a1915c5528ce5
vtrolinux <vtrolinux@gmail.com>
2022-01-04 12:24:15 -0300

    form login

--- frontend/src/components/pages/Auth/login.js
+++ frontend/src/components/pages/Auth/login.js
@@ -1,7 +1,38 @@
+import {useState, useContext} from 'react'
+import Input from '../../form/Input'
+import { Link } from 'react-router-dom'
+
+import styles from '../../form/Form.module.css'
+//context
+import {Context} from  '../../../context/UserContext'
+
 function Login(){
+
+    function handleOnChange(e){
+
+    }
+
     return (
-        <section>
+        <section className={styles.form_container}>
             <h1>Login</h1>
+            <form>
+                <Input
+                    text='E-mail'
+                    type='email'
+                    name='email'
+                    placeholder='Informe seu email'
+                    handleOnChange={handleOnChange}
+                />
+                <Input
+                    text='senha'
+                    type='password'
+                    name='password'
+                    placeholder='digite a sua senha'
+                    handleOnChange={handleOnChange}
+                />
+                <input type='submit' value='Entrar'/>
+            </form>
+            <p>Não tem conta? <Link to='/register'>Clique aqui.</Link></p>
         </section>
     )
 }
