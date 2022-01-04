21f62898baa04a8a98d339b585371ab5ffc624cb
vtrolinux <vtrolinux@gmail.com>
2022-01-04 05:15:45 -0300

    flash message component apply

--- frontend/src/components/layouts/Message.js
+++ frontend/src/components/layouts/Message.js
@@ -1,12 +1,32 @@
-import {useState} from 'react'
+import {useState , useEffect} from 'react'
+import bus from '../../utils/bus'
 import styles from './Message.module.css'
 
 function Message() {
-
+    const [visibility, setVisibility] = useState(false)//mostra-a apenas quando necessario
+    const [message, setMessage] = useState('')//text message
     const [type, setType] = useState('') //tipo de messagem
 
+    //permitir que o evento seja observado apenas uma vez, evitando loops infinitos
+    /*inicializacao da estrutura do useEffect, a ser observado L:13 },[] ), preenchendo o array posteriormente baseado no evento observado*/
+
+    useEffect(() => {
+        //bus > evento a ser observado
+        bus.addListener('flash', ({ message, type }) => {
+
+            setVisibility(true)
+            setMessage(message)
+            setType(type)
+
+            setTimeout(()=>{
+                setVisibility(false)
+            },3000)
+
+        })
+
+    },[] )
     return (
-        <div className={`${styles.message} ${styles.type}`}>minha mensagem</div>
+        visibility && ( <div className={`${styles.message} ${styles[type]}`}>{message}</div> )
     )
 }
 
--- frontend/src/hooks/useAuth.js
+++ frontend/src/hooks/useAuth.js
@@ -1,17 +1,30 @@
 import api from '../utils/api'
 import { useState, useEffect } from 'react'
 import { useHistory } from 'react-router-dom'
+import  useFlashMessage  from './useFlashMessage'
 
 export default function useAuth(){
+
+    const { setFlashMessage } = useFlashMessage()
+
     async function register(user){
+
+        let msgMessage = 'Cadastro realizado com sucesso'
+        let msgType = 'success'
         try {
             const data = await api.post('/auth/register', user).then((response) => {
                 return response.data
             })
+            
             console.log(data)
         } catch (error) {
-            console.log(error)
+            //console.log(error)
+            msgMessage = error.response.data.message
+            msgType = 'error'
         }
+
+        setFlashMessage(msgMessage, msgType)
+
     }
     return { register }
 }
