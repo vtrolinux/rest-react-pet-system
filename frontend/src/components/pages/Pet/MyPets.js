28143647f9e9b82591478fd47a39dba290503bcb
vtrolinux <vtrolinux@gmail.com>
2022-01-06 06:12:48 -0300

    my pets page +

--- frontend/src/components/pages/Pet/MyPets.js
+++ frontend/src/components/pages/Pet/MyPets.js
@@ -1,7 +1,17 @@
+import {useState, useEffect} from  'react'
+import {Link} from 'react-router-dom'
 function MyPets(){
+
+    const [pets, setPets] = useState([])
+
     return (
         <section>
             <h1>MyPets</h1>
+            <Link to='/pet/add'> Cadastrar Pet </Link>
+            <div>
+                {pets.length > 0 && ( <p>meus pets cadastrados </p> )}
+                {pets.length === 0 && ( <p>voce nao possui pets cadastrados </p> )}
+            </div>
         </section>
     )
 }
