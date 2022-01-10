0e49fb1004e0ce64d784caba7e69dd03aeea33f3
vtrolinux <vtrolinux@gmail.com>
2022-01-10 11:28:42 -0300

    GET request my pets

--- frontend/src/components/pages/Pet/MyPets.js
+++ frontend/src/components/pages/Pet/MyPets.js
@@ -1,8 +1,27 @@
 import {useState, useEffect} from  'react'
 import {Link} from 'react-router-dom'
+import api from '../../../utils/api'
+
+import RoundedImage from '../../layouts/RoundedImage'
+/* hooks */
+import useFlashMessage from '../../../hooks/useFlashMessage'
+
 function MyPets(){
 
     const [pets, setPets] = useState([])
+    const [token] = useState(localStorage.getItem('token') || '')
+    const {setFlashMessage} = useFlashMessage()
+
+    //x1
+    useEffect(() => {
+        api.get('/pets/mypets', {
+        headers: {
+          Authorization: `Bearer ${JSON.parse(token)}`,
+        }}).then((response) => {
+            console.log(response.data.pets)
+            setPets(response.data.pets)
+        })
+    },[token])//dependencia
 
     return (
         <section>
