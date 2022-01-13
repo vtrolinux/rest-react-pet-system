cb9ffdd6730744bd4b29b0fc7a85244abe39b9e9
vtrolinux <vtrolinux@gmail.com>
2022-01-13 11:40:09 -0300

    get petlist for home page

--- frontend/src/components/pages/Home.js
+++ frontend/src/components/pages/Home.js
@@ -1,4 +1,20 @@
+import api from '../../utils/api'
+import {Link} from  'react-router-dom'
+import {useState, useEffect} from 'react'
+import styles from './Home.module.css'
+
 function Home(){
+
+    const [petList, setPets] = useState([])
+
+    useEffect(() => {
+        api.get('/pets').then((response) => {
+            setPets(response.data.petList) 
+        }).catch((error) => {
+            
+        })
+    },[])
+
     return (
         <section>
             <h1>Home</h1>
