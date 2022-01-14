25aebb62e8fde8dbd20ee83c040627a0ed386363
vtrolinux <vtrolinux@gmail.com>
2022-01-14 13:58:43 -0300

    get pet details (useEffect)

--- frontend/src/components/pages/Pet/PetDetails.js
+++ frontend/src/components/pages/Pet/PetDetails.js
@@ -1,3 +1,4 @@
+import api from '../../../utils/api'
 import styles from './PetDetails.module.css'
 import { useState, useEffect } from 'react'
 import { useParams, Link } from 'react-router-dom'
@@ -8,11 +9,19 @@ import useFlashMessage from '../../../hooks/useFlashMessage'
 function PetDetails(){
 
     const [pet, setPet] = useState({})
-  const { id } = useParams()
+    const { id } = useParams()
+    const { setFlashMessage } = useFlashMessage()
+    const [token] = useState(localStorage.getItem('token') || '')
+
+    useEffect(() => {
+        api.get(`pets/${id}`).then((response) => {
+            setPet(response.data.pet)
+        })
+    },[id])
 
     return(
         <section>
-            <h1>Detalhes do Pet</h1>
+            <h1>Detalhes do Pet: {pet.name}</h1>
         </section>
     )
 }
